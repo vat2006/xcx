542aba099bdc342283f6fddd3e15732fc05e97c1
vat2006 <vat2006@msn.com>
2021-05-07 16:23:47 +0800

    5/7

--- control/ConnectAction.go
+++ control/ConnectAction.go
@@ -1,27 +1,38 @@
 package control
 
 import (
+	"bufio"
 	"crypto/sha1"
 	"encoding/base64"
 	"fmt"
 	"net"
+	"strings"
 )
 
-func ConnConnect(conn net.Conn, param map[string]string) {
-	key := param["Sec-WebSocket-Key"]
+func DoConnect(conn net.Conn, param map[string]string) (isConnected bool) {
 	sha := sha1.New()
-	sha.Write([]byte(key + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11"))
-	h := sha.Sum(nil)
-	newKey := base64.StdEncoding.EncodeToString(h)
-	msg := "HTTP/1.1 101 Switching Protocols\r\n"
-	msg = msg + "Upgrade: websocket\r\n"
-	msg = msg + "Sec-WebSocket-Version:" + param["Sec-WebSocket-Version"] + "\r\n"
-	msg = msg + "Connection: Upgrade\r\n"
-	msg = msg + "Sec-WebSocket-Accept: " + newKey + "\r\n\r\n"
-	fmt.Println(msg)
-	n, err := conn.Write([]byte(msg))
-	fmt.Println(n)
-	if err != nil {
-		fmt.Println(err)
+	sha.Write([]byte(strings.TrimSpace(param["Sec-WebSocket-Key"])))
+	sha.Write([]byte("258EAFA5-E914-47DA-95CA-C5AB0DC85B11"))
+	newKey := base64.StdEncoding.EncodeToString(sha.Sum(nil))
+	wbuf := bufio.NewWriter(conn)
+	//rbuf := bufio.NewReader(conn)
+	wbuf.WriteString("HTTP/1.1 101 Switching Protocols\r\n")
+	wbuf.WriteString("Upgrade: websocket\r\n")
+	wbuf.WriteString(fmt.Sprintf("Sec-WebSocket-Version:%s\r\n", param["Sec-WebSocket-Version"]))
+	wbuf.WriteString("Connection: Upgrade\r\n")
+	wbuf.WriteString(fmt.Sprintf("Sec-WebSocket-Accept:%s\r\n\r\n", newKey))
+	wbuf.Flush()
+	rb := make([]byte, 1024)
+	var n int
+	for {
+		n, _ = conn.Read(rb)
+		if n > 0 {
+			break
+		}
 	}
+	fmt.Println(rb[0:n])
+	return true
+}
+func deCode(bs []byte) {
+
 }
--- libs/server.go
+++ libs/server.go
@@ -15,19 +15,22 @@ type Server struct {
 func (self *Server) Start() {
 	addr, _ := net.ResolveTCPAddr("tcp", "127.0.0.1:8282")
 	self.listener, _ = net.ListenTCP("tcp", addr)
-	defer self.listener.Close()
+	mainInChan := make(chan map[string]string, 2048)
+	mainOutChan := make(chan map[string]string, 2048)
 	for {
 		client, _ := self.listener.Accept()
-		go self.handler(client)
+		go self.handler(client, mainInChan, mainOutChan)
 	}
 }
-func (self *Server) handler(conn net.Conn) {
+func (self *Server) handler(conn net.Conn, outChan chan map[string]string, inChan chan map[string]string) {
 	b := make([]byte, 1024)
 	n, _ := conn.Read(b)
 	if n <= 9 {
 		control.ConnClose(conn)
 	}
-	control.ConnConnect(conn, parser(string(b[0:n])))
+	if control.DoConnect(conn, parser(string(b[0:n]))) {
+
+	}
 	time.Sleep(10000)
 }
 func parser(s string) (param map[string]string) {
@@ -37,13 +40,13 @@ func parser(s string) (param map[string]string) {
 	}
 	param = make(map[string]string)
 	for _, v := range ss {
-		ps = strings.Split(v, ":")
+		ps = strings.SplitN(v, ":", 2)
 		if len(ps) == 1 {
 			pps := strings.Fields(ps[0])
 			if len(pps) > 1 {
 				switch pps[0] {
 				case "GET":
-					param["Type"] = pps[0]
+					param["Method"] = pps[0]
 					param["Path"] = pps[1]
 					param["Version"] = pps[2]
 				}
